52ae26cedb935c2d0feaef665656af82a68b6b3c
br and tf <br>
2016-03-15 09:57:06 -0700

    finish ping pong and weather app

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -21,3 +21,23 @@ $(document).ready(function(){
     $('#solution').prepend('<p>Thank You, ' + email + ' has been added to our list!</p>');
   });
 });
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function(){
+  $('#weatherLocation').click(function(){
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response)
+    {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + '%');
+    }).fail(function(error){
+      $('.showWeather').text(error.message);
+    });
+  });
+});
